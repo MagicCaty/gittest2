7a6afda703fe7853acd5d6f3d9fd61985c980846
MagicCat <1165616464@qq.com>
2022-01-25 13:15:45 +0800

    THRIED4

--- src/main/java/com/test1.java
+++ src/main/java/com/test1.java
@@ -6,6 +6,7 @@ public class test1 {
         System.out.println("test");
         System.out.println("test1");
         System.out.println("test11231231");
-        System.out.println("test11231231");
+        System.out.println("test11231231666");
+        System.out.println("test11231231666");
     }
 }
